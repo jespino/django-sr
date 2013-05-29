cd18c6a53962ebcd48968ede9d7c129f2062ef48
Jesús Espino <jesus.espino@kaleidos.net>
2013-05-29 21:47:19 +0200

    Adding django-jinja support

--- sr/templatetags/sr.py
+++ sr/templatetags/sr.py
@@ -1,8 +1,19 @@
-from sr import sr
+from sr import sr as sr_func
 
 from django import template
 register = template.Library()
 
+
 @register.simple_tag(name='sr')
 def sr_tag(key, *args, **kwargs):
-    return sr(key, *args, **kwargs)
+    return sr_func(key, *args, **kwargs)
+
+try:
+    from django_jinja.base import Library
+    jinja_register = Library()
+
+    @jinja_register.global_function
+    def sr(key, *args, **kwargs):
+        return sr_func(key, *args, **kwargs)
+except ImportError:
+    pass
