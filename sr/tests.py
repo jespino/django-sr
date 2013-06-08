ddc398e15824a4539191a986b0024ad99e302955
Andrey Antukh <niwi@niwi.be>
2013-06-08 20:11:23 +0200

    Remove usage of assertRaisesRegexp due deprecation warning.

--- sr/tests.py
+++ sr/tests.py
@@ -1,4 +1,10 @@
-from django.utils import unittest
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+import sys
+
 from django.conf import settings
 from django.template import Template, Context
 from django_jinja.base import env
@@ -6,14 +12,20 @@ from django_jinja.base import env
 from sr import sr
 from sr.templatetags.sr import *
 
+
+# Python 2.7 compatibility fix
+if sys.version_info[0] < 3:
+    unittest.TestCase.assertRaisesRegex = unittest.TestCase.assertRaisesRegexp
+
+
 class TestSequenceFunctions(unittest.TestCase):
     def test_sr_not_valid_key(self):
-        self.assertRaisesRegexp(Exception, "Not valid key: not-valid-key.not-valid-subkey", sr, "not-valid-key.not-valid-subkey")
-        self.assertRaisesRegexp(Exception, "Not valid key: test1.not-valid-subkey", sr, "test1.not-valid-subkey")
-        self.assertRaisesRegexp(Exception, "Not valid key: test2.test3.test4", sr, "test2.test3.test4")
+        self.assertRaisesRegex(Exception, "Not valid key: not-valid-key.not-valid-subkey", sr, "not-valid-key.not-valid-subkey")
+        self.assertRaisesRegex(Exception, "Not valid key: test1.not-valid-subkey", sr, "test1.not-valid-subkey")
+        self.assertRaisesRegex(Exception, "Not valid key: test2.test3.test4", sr, "test2.test3.test4")
 
     def test_sr_not_valid_params(self):
-        self.assertRaisesRegexp(Exception, "Not valid parameters for key test4.test4", sr, "test4.test4", "param")
+        self.assertRaisesRegex(Exception, "Not valid parameters for key test4.test4", sr, "test4.test4", "param")
 
     def test_sr_valid_key(self):
         self.assertEqual(sr('test1'), 'Test1')
